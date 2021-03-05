cb11622daf79f5d4dfae4419df74c657211b825e
luiseduardo69 <72415551+luiseduardo69@users.noreply.github.com>
2021-03-04 23:44:55 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,17 @@
+from tipocliente  import TipoCliente
+
+class Cliente():
+    def __init__(self, id, nome, codigo, cnpjcpf, tipo):
+        self._id = id
+        self._nome = nome
+        self._codigo = codigo
+        self._cnpjcpf = cnpjcpf
+        self._tipo = tipo
+        
+    def str(self):
+        string="\nId={4} Codigo={2} Nome={3} CNPJ/CPF={1} Tipo={0}".format(self._tipo, self._cnpjcpf, self._codigo, self._nome, self._id)
+        return string
+    
+if __name__ == '__main__':
+    cliente=Cliente(1, "Jose simão da Silva", 100, '620.150.375-37', TipoCliente.PESSOA_FISICA)
+    print(cliente.str())
---
+++ notafiscal.py
@@ -0,0 +1,38 @@
+import datetime
+from cliente        import Cliente
+from produto        import Produto
+from itemnotafiscal import ItemNotaFiscal
+
+
+class NotaFiscal():         
+    def __init__(self, Id, codigo, cliente):
+        self._Id = Id
+        self._codigo=codigo
+        self._cliente=cliente 
+        self._data=datetime.datetime.today()   
+        self._itens=[]
+        self._valorNota=0.0        
+        
+    def setCliente(self, cliente):
+        if isinstance(cliente, Cliente):
+            self._cliente=cliente    
+            
+    def adicionarItem(self, item): 
+        if isinstance(item, ItemNotaFiscal):
+            self._itens.append(item)
+        
+    def calcularNotaFiscal(self):
+        valor=0.0
+        for item in self._itens:
+            valor += item._valorItem
+        self.valorNota=valor
+
+
+    def imprimirNotaFiscal(self):
+        print('-' * 112, "\nNOTA FISCAL {: > 92}/{}/{}".format(self._data.day, self._data.month, self._data.year))
+        print("Cliente:  {}\tNome:  {} \nCNJP/CPF:{}".format(self._Id, self._cliente._nome, self._cliente._cnpjcpf))
+        print("{}\nITENS\n{}".format('-' * 112,'-' * 112,))
+        print("Seq  Descrição{:<10} QTD\tValor Unit{:<22}Preço\n{} {}    {}\t{} {:>29}".format('\t' * 6, ' ' ,'-' * 4, '-' * 51, '-' * 5, '-' * 10, '-' * 10))
+        for a in self._itens:
+            print("{:0>3}  {:<53}{:>5} \t {:>6.2f} {:>30.2f}".format(a.getSequencial(), a.getDescricao(), a.getQuantidade(), a.getValorUnitario(), a.getValorit()))
+        print('_' * 112)
